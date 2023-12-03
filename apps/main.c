2215f70d9f612a20ef0a7c58de621c4f9866e530
Chong Liu <liuc12@miamioh.edu>
2023-12-03 13:36:46 +0800

    update

--- apps/main.c
+++ apps/main.c
@@ -2,7 +2,16 @@
 #include "lib_calc.h"
 
 int main(int argc, const char* argv[]) {
+    /* 打印 Hello World! */
+    printf("Hello World!\r\n");
+    printf("argc: %d \r\n", argc);
+    for (int i = 0; i < argc; i++) {
+        printf("argv[%d]: %s \r\n", i, argv[i]);
+    }
+
+    /* 调用 lib_calc 库中的函数 */
+    printf("==============================\r\n");
     int res = add(1, 2);
-    printf("res: %d \r\n", res);
+    printf("add result: %d \r\n", res);
     return 0;
 }
